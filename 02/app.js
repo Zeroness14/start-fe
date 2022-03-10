0b9ed3124236b03561a5ae0d0a44d745355da616
박병민 <qkrqudlas@gmail.com>
2022-03-10 16:38:33 +0900

    start-fe

---
+++ 02/app.js
@@ -0,0 +1,23 @@
+var name = 'aj';
+name = 111;
+console.log(name);
+
+var isMan = 'true';
+var isMan1 = true;
+console.log(isMan);
+
+//object
+
+var img = {};
+img.width = 100;
+img.height = 200;
+console.log(img);
+
+// 배열
+
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+// 연산자
+console.log(1+1);
+console.log(name + 'hello');
