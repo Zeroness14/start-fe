3e047a7cbc236351e16fea514e22638ab310f985
hyunsoo <qkrqudlas@gmail.com>
2022-05-19 17:45:26 +0900

    start-fe

---
+++ 12/class/app.js
@@ -0,0 +1,4 @@
+import sum1 from '../src/sum.js';
+import {sum2} from '../src/sum.js'
+console.log(sum1(1,2));
+console.log(sum2(1,2));
---
+++ 12/src/index.js
@@ -0,0 +1,9 @@
+/*
+import sum1 from './sum';
+import { sum2 } from './sum';
+
+console.log(sum1(1, 2));
+console.log(sum2(2, 2));
+*/
+
+const queryString = require('querystring')
